a890a0d416595e8fbe6f8e5afa326e6cdd13f080
Carlos Molmelstet <molmelstet2016@gmail,com>
2020-10-31 19:01:32 -0300

    commit 1

---
+++ js/config.js
@@ -0,0 +1,94 @@
+$(function() {
+	simpleCart({
+
+	    // array representing the format and columns of the cart, see
+	    // the cart columns documentation
+	    cartColumns: [
+	        { attr: "name" , label: "Nome" },
+	        { attr: "price" , label: "Preço", view: 'currency' },
+			{ attr: "size" , label: "Size"},
+			{ view: "decrement" , label: false },
+	        { attr: "quantity" , label: "Quantidade" },
+	        { view: "increment" , label: false },
+	        { attr: "total" , label: "SubTotal", view: 'currency' },
+	        { view: "remove" , text: "Remover" , label: false }
+	    ],
+
+	    // "div" or "table" - builds the cart as a table or collection of divs
+	    cartStyle: "div",
+
+	    // how simpleCart should checkout, see the checkout reference for more info
+	    checkout: {
+	        type: "PayPal" ,
+			email: "you@yours.com",
+			
+			sandbox: true
+
+	    },
+
+	    // set the currency, see the currency reference for more info
+	    currency: "BRL",
+
+	    // collection of arbitrary data you may want to store with the cart,
+	    // such as customer info
+	    data: {},
+
+	    // set the cart langauge (may be used for checkout)
+	    language: "portugues-br",
+
+	    // array of item fields that will not be sent to checkout
+	    excludeFromCheckout: [
+	    ],
+
+	    // custom function to add shipping cost
+	    shippingCustom: null,
+
+	    // flat rate shipping option
+	    shippingFlatRate: 0.1,
+
+	    // added shipping based on this value multiplied by the cart quantity
+	    shippingQuantityRate: 0,
+
+	    // added shipping based on this value multiplied by the cart subtotal
+	    shippingTotalRate: 0,
+
+	    // tax rate applied to cart subtotal
+	    taxRate: 0,
+
+	    // true if tax should be applied to shipping
+	    taxShipping: false,
+
+	    // event callbacks
+	    beforeAdd               	: null,
+	    afterAdd                	: null,
+	    load                    	: null,
+	    beforeSave              	: null,
+	    afterSave               	: null,
+	    update                  	: null,
+	    ready                   	: null,
+	    checkoutSuccess             : null,
+	    checkoutFail                : null,
+	    beforeCheckout              : simpleCart.bind( 'beforeCheckout' , function( data ){
+			data.invoiceNumber = "ABC-123456789";
+		  })
+
+	});
+
+	simpleStore.init({
+
+		// brand can be text or image URL
+		brand : "Shop",
+
+		// numder of products per row (accepts 1, 2 or 3)
+		numColumns : 3,
+
+		// name of JSON file, located in directory root
+		JSONFile : "products.json"
+
+	});
+
+	simpleCart.bind( "afterAdd" , function( item ){
+		alert( item.get("name") + " foi adicionada ao carrinho!" );
+	  });
+
+});
